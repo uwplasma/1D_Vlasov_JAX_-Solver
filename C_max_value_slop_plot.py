eb79721bc534bc90f4003c5a350f5eda2e449b0b
XYJeff23 <158072776+XYJeff23@users.noreply.github.com>
2024-12-10 07:56:06 +0800

    Update C_max_value_slop_plot.py

--- C_max_value_slop_plot.py
+++ C_max_value_slop_plot.py
@@ -9,49 +9,53 @@ import matplotlib.pyplot as plt
 from jax.experimental.ode import odeint
 import pandas as pd
 from scipy.stats import linregress
+
 jax.config.update("jax_enable_x64", True)
 
-m_max = 40
-t_span = jnp.linspace(0, 10, 100)
-k_values = jnp.linspace(0.01, 1, 100)
-m = 9.1093837e-31
-q = 1.602e-19
-e = 8.8541878188e-12
-w = jnp.sqrt(jnp.sqrt(jnp.pi)*q**2/m/e)
+
+m_max = 400
+t_span = jnp.linspace(0, 20, 2001)
+k_values = jnp.linspace(0.1, 1, 100)
+nu = 0
 # C_values = w/k_values/1j
 # print(C_values)
 # print(w)
 
+
 def objective(f, t, k, v_e):
+    # dfm_dt = jnp.zeros_like(f)
     indices = jnp.arange(len(f))
-    dfm_dt = (-1j*jnp.sqrt(2)*k * (jnp.roll(f, 1)) * jnp.sqrt((jnp.roll(indices, 1) / 2)+
-              jnp.sqrt(indices / 2) * jnp.roll(f, -1) + v_e*f))
-    dfm_dt = (dfm_dt.at[1].add(1/(k*1j)*f[0]))
-    dfm_dt = dfm_dt.at[0].set(-1j*jnp.sqrt(2)*k*(f[1] * jnp.sqrt(1 / 2)+v_e*f[0]))
-    dfm_dt = dfm_dt.at[-1].set((-1j*jnp.sqrt(2)*k*(jnp.sqrt(indices[-1] / 2) * f[-2]+v_e*f[-1])))
+    dfm_dt = -1j * jnp.sqrt(2) * k * (jnp.roll(f, 1) * jnp.sqrt((indices + 1) / 2) +
+              jnp.sqrt(indices / 2) * jnp.roll(f, -1) + v_e * f)
+    dfm_dt = dfm_dt.at[1].add(-(1j / k) * f[0])
+    dfm_dt = dfm_dt.at[0].set(-1j * jnp.sqrt(2) * k * (f[1] * jnp.sqrt(1 / 2) + v_e * f[0]))
+    dfm_dt = dfm_dt.at[-1].set(-1j * jnp.sqrt(2) * k * (jnp.sqrt(indices[-1] / 2) * f[-2] + v_e * f[-1]))
+    # dfm_dt = dfm_dt.at[3:].add( - nu * (indices[3:] * (indices[3:] - 1) * (indices[3:] - 2)) / ((m_max - 1) * (m_max - 2) * (m_max - 3)) * f[3:])
     return dfm_dt
 
-def solve_ode(k, t, m_max,v_e):
+
+def solve_ode(k, t, m_max, v_e):
     y0 = jnp.zeros(m_max, dtype=jnp.complex128)
     y0 = y0.at[0].set(1)
-    solution = odeint(objective, y0, t, k,v_e)
+    solution = odeint(objective, y0, t, k, v_e)
     return jnp.log(jnp.abs(solution))
 
 
 v_e = 0
-
 slopes = []
 
 mathematica_data = pd.read_csv('damping_rate.csv')
+# mathematica_data_2 = pd.read_csv('C:\\Cristian\\Postdoc\\Madison\\Code\\damping_rate_1000000.csv')
 
 # for k in k_values:
 #     solution = solve_ode(k, t_span, m_max, v_e)
 #     solution = solution[:, 0]
 #     plt.plot(t_span, solution)
-#     # plt.legend()
-#     plt.show()
-
-
+#     plt.legend()
+    
+# plt.show()
+    
+    
 for k in k_values:
     solution = solve_ode(k, t_span, m_max, v_e)
     solution = solution[:, 0]
@@ -61,28 +65,30 @@ for k in k_values:
     local_maxima_values = solution[local_maxima_indices]
     local_maxima_times = t_span[local_maxima_indices]
 
-    # slops
+    # slopes
     if len(local_maxima_times) > 1:  # fit for graph have 2+ mix local max
         slope, intercept, r_value, p_value, std_err = linregress(local_maxima_times, local_maxima_values)
         slopes.append(slope)
 
-        # plot fit for each C
-        fit_line = slope * local_maxima_times + intercept
+        # # plot fit for each C
+        # fit_line = slope * local_maxima_times + intercept
         # plt.plot(local_maxima_times, fit_line, 'r--', label=f'Fit Line: slope={slope:.2f}')
         # plt.plot(t_span, solution, label=f'K = {k:.2f}')
         # plt.legend()
         # plt.show()
 
-# plot c value and slope
+#
+
+# # plot k value and slope
+
 plt.figure()
-plt.plot(k_values, slopes, '-o', label='Slope vs. k')
-plt.plot(mathematica_data.values[:80, 0], mathematica_data.values[:80, 1], '-x', label='Mathematica Slope vs. k')
-plt.xlabel('C/k value')
+plt.plot(k_values, jnp.array(slopes), '-o', label='Slope vs. k')
+plt.plot(mathematica_data.values[:, 0], mathematica_data.values[:, 1], '-', label='Mathematica Slope vs. k')
+# plt.plot(mathematica_data_2.values[:, 0], mathematica_data_2.values[:, 1], '-r', label='Mathematica Slope vs. k')
+plt.xlim(0.0, 1)
+plt.ylim(-1, 0.01)
+plt.xlabel('k')
 plt.ylabel('Slope at Local Maxima')
 plt.title('Slope vs. k Value')
 plt.legend()
-plt.xlim(0,0.6)
-plt.ylim(-0.2,0.1)
 plt.show()
-
-
